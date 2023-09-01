ca5674d6ab60bd6e90149a608f1cc662484ddbe4
Wholeheartedly <119653204+AESMatias@users.noreply.github.com>
2023-09-01 06:28:48 -0400

    Updating the output

--- file_reader.py
+++ file_reader.py
@@ -1,3 +1,7 @@
+# This script read a .txt file with a lot of rows.
+# Also includes a RegEx expression filter to remove the unnecessary spaces
+# reducing it to 1 at very least.
+
 import re
 import csv
 import os
@@ -5,6 +9,9 @@ import sys
 path = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(path)
 file_name = 'inglesss.txt'
+first_filter = "Aprobado"
+second_filter = "Reprobado"
+approved, failed = 0, 0
 
 # ingles_file = open('inglesss.txt', 'r')
 # lines = [line for line in ingles_file]
@@ -22,24 +29,22 @@ reader = csv.reader(file)
 
 header = next(reader)  # The first line is the header
 data = [row for row in reader]
-print('HEADER', data[0])
-print('-'*50)
-Aprobados = 0
-Reprobados = 0
+# print('HEADER', data[0])
+# print('-'*50)
 for i in range(4, len(data)):
-    print('+'*80)
+    # print('+'*80)
     line_data = str(data[i]).strip('[]').strip("'").strip(' ').split(',')[0]
     # This RegEx remove extra spaces
     line_data = re.sub(r'\s{2,}', ' ', line_data)
     line_data = line_data.split(' ')
-    print(line_data)
+    # print(line_data)
     for line in line_data:
-        print(line)
-        if "Aprobado" in line:
-            Aprobados += 1
-        elif "Reprobado" in line:
-            Reprobados += 1
-print('-'*80)
-print('Aprobados: ', Aprobados, ' '*20, '|')
-print('Reprobados: ', Reprobados, ' '*19, '|')
-print('-'*80)
+        # print(line)
+        if first_filter in line:
+            approved += 1
+        elif second_filter in line:
+            failed += 1
+print('-'*50)
+print('Approved: ', approved, ' '*4, '|')
+print('Failed: ', failed, ' '*6, '|')
+print('-'*50)
